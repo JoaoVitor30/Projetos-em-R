958e433640c4fe3c2cc51e7e3cd47434cc2b62e4
João Vitor <joaopersil@hotmail.com>
2021-06-20 16:31:46 -0300

    Sequencia e Repetição de elementos

---
+++ Objetos/Sequencias/Sequencia_e_Repetição.R
@@ -0,0 +1,12 @@
+# Sequencia de elementos
+1:35
+seq(1,35)
+
+# Sequencia de elementos de 2 em 2
+seq(1,35,2)
+
+# Repetição de vetor
+rep(salario,times=2,each=3)
+
+# Repetição de elementos
+rep(1:35,2)
