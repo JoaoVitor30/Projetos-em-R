b501528b5e4b72390c66fe65a4f09961beeb3850
João Vitor <joaopersil@hotmail.com>
2021-06-30 12:14:29 -0300

    Nomear as dimensões

---
+++ Objetos/Array/Nomear.R
@@ -0,0 +1,6 @@
+# Nomeando as linhas,colunas e as matrizes
+
+P<-array(0, dim = c(3,2,2), dimnames = list(nome_linha,nome_coluna,nome_matriz))
+nome_linha<-c("L1","L2","L3")
+nome_coluna<-c("C1","C2")
+nome_matriz<-c("M1","M2")
