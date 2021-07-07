e1d97bad5f3392624c45edfdec427a3c5416ccb7
João Vitor <joaopersil@hotmail.com>
2021-07-07 12:31:15 -0300

    Acesso

---
+++ Objetos/Data Frame/PesquisaDataFrame.R
@@ -0,0 +1,11 @@
+# Filtragem de elementos
+dados[Mercado=="Extra",]
+dados[dados$Mercado=="Extra",]
+
+# Pesquisando por indice ou pela variavel
+dados[[1]]
+dados$fornecedores
+dados[2,3]
+dados$Janeiro[2]
+dados$Março[c(3,4)]
+dados[c(1,2),]
